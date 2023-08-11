43239bea704d0c7431664cbf1858f12282e2a542
Ganaa Amarsaikhan <Gantulga Amarsaikhan>
2023-08-11 15:21:32 +0800

    users table migration, api example

--- database/migrations/2023_08_11_064851_create_users_table.php
+++ database/migrations/2023_08_11_064851_create_users_table.php
@@ -14,8 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone', 100)->nullable();           
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->tinyText('address')->nullable();
+            $table->date('birthday')->nullable();
+            $table->char('gender', 10)->nullable();
+            $table->bigInteger('role_id');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
