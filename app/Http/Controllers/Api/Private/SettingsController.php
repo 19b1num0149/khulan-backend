4d652e342c47ccbf3b81afc9fe802efb82da1b61
“bamartvwshim0327” <bamartvwshim0327@gmail.com>
2023-08-18 16:59:42 +0800

    Amaraa

---
+++ app/Http/Controllers/Api/Private/SettingsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\Private;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Request\Api\Private\UserSettingsRequest;
+use App\Models\UserSettings;
+
+class SettingsController extends Controller{
+
+    public function getData(Request $request) {
+
+        $data = UserSettings::where('user_id' , $request->user()->id)->get();
+
+        return response()->json(['msg' => 'success' , 'data' => $data] , 200);
+    }
+
+    public function store(UserSettingsRequest $request) {
+
+        $data = UserSettings::where('user_id' ,$request->user()->id)->first();
+        $data->event_off = $request->event_off;
+        $data->content_on = $request->content_on;
+        $data->coupon = $request->coupon;
+        $data->save();
+
+        return response()->json(['msg' => 'Амжилттай хадгаллаа'] , 200);
+
+    }
+}
--- routes/api/private.php
+++ routes/api/private.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Api\Private\ProfileController;
 use App\Http\Controllers\Api\Private\InterestController;
 use App\Http\Controllers\Api\Private\GroupController;
 
+use App\Http\Controllers\Api\Private\SettingsController;
+use App\Http\Controllers\Api\Private\NotificationController;
+use App\Http\Controllers\Api\Private\PaymentController;
+use App\Http\Controllers\Api\Private\ContentController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -14,8 +18,24 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/authuser/{id}' , [ProfileController::class , 'getUser']);
     Route::post('/profile/{id}' , [ProfileController::class , 'postUser']);
     Route::get('/user-interest/{user_id}' , [InterestController::class , 'getUserInterest']);
-    Route::post('/add-user-interest/{user_id}' , [InterestController::class , 'postUserInterest']);
+        Route::post('/add-user-interest/{user_id}' , [InterestController::class , 'postUserInterest']);
     Route::get('/user-groups/{user_id}' , [GroupController::class , 'getUserGroups']);
     Route::get('/user/{user_id}/group/{group_id}/joined' , [GroupController::class , 'getJoinedGroupOfUser']);
     Route::post('/join-request/user/{user_id}/group/{group_id}' , [GroupController::class , 'createGroupJoinRequest']);
+    // Settings
+    Route::get('/user/settings' , [SettingsController::class , 'getData']);
+    Route::post('/user/settings' , [SettingsController::class , 'store']);
+
+    // Notification
+    Route::get('/user/notification' , [NotificationController::class , 'getData']);
+    Route::post('/user/notification/{id}' , [NotificationController::class , 'read']);
+
+    // Payment
+    Route::get('/user/payment' , [PaymentController::class , 'getData']);
+    Route::get('/user/payment/{id}' , [PaymentController::class , 'view']);
+
+    // Content
+    Route::get('/user/content/{id}' , [ContentController::class , 'getData']);
+
+
 });
