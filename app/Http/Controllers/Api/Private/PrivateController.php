7cc1f7f2d9a40db1154b0f309201384b8f03985e
sundui2016 <sundui2016@gmail.com>
2023-08-18 17:34:40 +0800

    sundui

--- app/Http/Controllers/Api/Private/PrivateController.php
+++ app/Http/Controllers/Api/Private/PrivateController.php
@@ -3,6 +3,7 @@
 namespace app\Http\Controllers\Api\Private;
 
 use App\Http\Controllers\Controller;
+use App\Models\GroupJoinRequest;
 use App\Models\GroupMember;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -10,88 +11,5 @@ use App\Models\UserInterest;
 
 class PrivateController extends Controller
 {
-    public function getUser($id)
-    {
-
-        $user = User::find($id);
-        if (!isset($user)) {
-            return response()->json(['msg' => 'User not found'], 404);
-        }
-        return response()->json(['msg' => 'Success', 'data' => $user], 200);
-    }
-
-    public function postUser(Request $request, $id)
-    {
-        $user = User::find($id);
-    
-        $data = $request->validate([
-            'name' => 'required|string',
-            'email' => 'required|email|unique:users',
-            'phone' => 'required|string|unique:users',
-            'address' => 'nullable|string',
-            'age' => 'nullable|integer|min:0',
-            'gender' => 'nullable|in:male,female,other',
-        ]);
-    
-        $user->update($data);
-    
-        return response()->json(['msg' => 'success', 'data' => $user], 200);
-    }
-    
-
-    public function getUserInterest($user_id)
-    {
-        $userInterest = UserInterest::where('user_id', $user_id)->get(); 
-    
-        if ($userInterest->isEmpty()) {
-            return response()->json(['msg' => 'User interest not found'], 404);
-        }
-    
-        return response()->json(['msg' => 'Success', 'data' => $userInterest], 200);
-    }
-    
-
-    public function postUserInterest(Request $request, $user_id)
-    {
-        {
-            $data = $request->validate([
-                'interest_id' => 'required|integer',
-            ]);
-    
-            $userInterest = new UserInterest();
-            $userInterest->user_id = $user_id;
-            $userInterest->interest_id = $data['interest_id'];
-            
-            $userInterest->save();
-            
-            return response()->json(['msg' => 'Success', 'data' => $userInterest], 200);
-        }
-
-    }
-
-    public function getUserGroups($user_id)
-    {
-        $userGroups = GroupMember::where('member_id', $user_id)->get(); 
-        if ($userGroups->isEmpty()) {
-            return response()->json(['msg' => 'Хэрэглэгч бүлгэмд элсээгүй байна.'], 404);
-        }
-    
-        return response()->json(['msg' => 'Success', 'data' => $userGroups], 200);
-    }
-    
-    public function getJoinedGroupOfUser($user_id, $group_id)
-    {
-        $joinedGroup = GroupMember::where('member_id', $user_id)
-        ->where('group_id', $group_id)
-        ->first();
-        
-
-        if ($joinedGroup === null) {
-            return response()->json(['msg' => 'Хэрэглэгч уг бүлгэмд элсээгүй байна.'], 404);
-        }
-            
-        return response()->json(['msg' => 'Success', 'data' => $joinedGroup], 200);
-    }
-    
     
 }
--- routes/api/private.php
+++ routes/api/private.php
@@ -2,16 +2,20 @@
 
 use App\Http\Controllers\Api\Guest\LoginController;
 use App\Http\Controllers\Api\Private\PrivateController;
+use App\Http\Controllers\Api\Private\ProfileController;
+use App\Http\Controllers\Api\Private\InterestController;
+use App\Http\Controllers\Api\Private\GroupController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [LoginController::class, 'logout']);
-    Route::get('/authuser/{id}' , [PrivateController::class , 'getUser']);
-    Route::post('/profile/{id}' , [PrivateController::class , 'postUser']);
-    Route::get('/user-interest/{user_id}' , [PrivateController::class , 'getUserInterest']);
-    Route::post('/add-user-interest/{user_id}' , [PrivateController::class , 'postUserInterest']);
-    Route::get('/user-groups/{user_id}' , [PrivateController::class , 'getUserGroups']);
-    Route::get('/user/{user_id}/group/{group_id}/joined' , [PrivateController::class , 'getJoinedGroupOfUser']);
-
+    Route::get('/authuser/{id}' , [ProfileController::class , 'getUser']);
+    Route::post('/profile/{id}' , [ProfileController::class , 'postUser']);
+    Route::get('/user-interest/{user_id}' , [InterestController::class , 'getUserInterest']);
+    Route::post('/add-user-interest/{user_id}' , [InterestController::class , 'postUserInterest']);
+    Route::get('/user-groups/{user_id}' , [GroupController::class , 'getUserGroups']);
+    Route::get('/user/{user_id}/group/{group_id}/joined' , [GroupController::class , 'getJoinedGroupOfUser']);
+    Route::post('/join-request/user/{user_id}/group/{group_id}' , [GroupController::class , 'createGroupJoinRequest']);
 });
