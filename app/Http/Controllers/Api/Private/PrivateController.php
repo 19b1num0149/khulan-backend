e8d7222b9d44a95bb1e8285bfff7d678a4431bc5
sundui2016 <sundui2016@gmail.com>
2023-08-18 17:36:57 +0800

    sundui

--- app/Http/Controllers/Api/Private/PrivateController.php
+++ app/Http/Controllers/Api/Private/PrivateController.php
@@ -1,15 +1,18 @@
 <?php
-
 namespace app\Http\Controllers\Api\Private;
-
 use App\Http\Controllers\Controller;
-use App\Models\GroupJoinRequest;
-use App\Models\GroupMember;
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\UserInterest;
 
 class PrivateController extends Controller
 {
-    
+    public function getUser($id){
+
+        $user = User::find($id);
+        if (!isset($user)) {
+            return response()->json(['msg' => 'User not found'], 404);
+        }
+        return response()->json(['msg' => 'Success' , 'data' => $user], 200);
+
+    }
 }
