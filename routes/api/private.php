99bd83aea8b3bddcfbda1849425a45ed27ca84e2
“bamartvwshim0327” <bamartvwshim0327@gmail.com>
2023-08-18 16:59:42 +0800

    Amaraa

--- routes/api/private.php
+++ routes/api/private.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\Api\Guest\LoginController;
 use App\Http\Controllers\Api\Private\PrivateController;
+use App\Http\Controllers\Api\Private\SettingsController;
+use App\Http\Controllers\Api\Private\NotificationController;
+use App\Http\Controllers\Api\Private\PaymentController;
+use App\Http\Controllers\Api\Private\ContentController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -9,5 +13,21 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [LoginController::class, 'logout']);
 
     Route::get('/authuser/{id}' , [PrivateController::class , 'getUser']);
+    
+    // Settings
+    Route::get('/user/settings' , [SettingsController::class , 'getData']);
+    Route::post('/user/settings' , [SettingsController::class , 'store']);
+
+    // Notification
+    Route::get('/user/notification' , [NotificationController::class , 'getData']);
+    Route::post('/user/notification/{id}' , [NotificationController::class , 'read']);
+
+    // Payment
+    Route::get('/user/payment' , [PaymentController::class , 'getData']);
+    Route::get('/user/payment/{id}' , [PaymentController::class , 'view']);
+
+    // Content
+    Route::get('/user/content/{id}' , [ContentController::class , 'getData']);
+
 
 });
