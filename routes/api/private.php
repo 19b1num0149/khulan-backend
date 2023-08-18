ee3dd57e0e2f26ecc0f066c9f6d1503396169c4e
sundui2016 <sundui2016@gmail.com>
2023-08-18 18:27:41 +0800

    sundui

--- routes/api/private.php
+++ routes/api/private.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/authuser/{id}' , [ProfileController::class , 'getUser']);
     Route::post('/profile/{id}' , [ProfileController::class , 'postUser']);
     Route::get('/user-interest/{user_id}' , [InterestController::class , 'getUserInterest']);
-        Route::post('/add-user-interest/{user_id}' , [InterestController::class , 'postUserInterest']);
+    Route::post('/add-user-interest/{user_id}' , [InterestController::class , 'postUserInterest']);
     Route::get('/user-groups/{user_id}' , [GroupController::class , 'getUserGroups']);
     Route::get('/user/{user_id}/group/{group_id}/joined' , [GroupController::class , 'getJoinedGroupOfUser']);
     Route::post('/join-request/user/{user_id}/group/{group_id}' , [GroupController::class , 'createGroupJoinRequest']);
